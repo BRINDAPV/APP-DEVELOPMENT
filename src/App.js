01260ac5ebda14fe56d869931f8f0f9d5c6fc086
BRINDAPV <124986873+BRINDAPV@users.noreply.github.com>
2024-07-24 09:42:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Profile from './components/Profile';
+import Navbar from './components/Navbar';
+import './components/Login.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">Welcome to the React App!</h1>
+      <p className="lead">This is a simple React application with routing and Bootstrap for styling.</p>
+    </div>
+  );
+}
+
+export default Home;
